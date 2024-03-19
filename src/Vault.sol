7a8631ed748c22c36271ca9f95203f2dfecfd3ee
Rory Hellyer <rory@webmodularity.com>
2024-03-19 08:14:25 -0700

    testing

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,20 +1,55 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+
 import "solmate/tokens/ERC4626.sol";
 import "solmate/auth/Owned.sol";
+import "solmate/tokens/ERC20.sol";
+import "solmate/utils/SafeTransferLib.sol";
+import {ISuperfluid, ISuperToken} from "superfluid-contracts/interfaces/superfluid/ISuperfluid.sol";
+import {ISuperTokenFactory} from "superfluid-contracts/interfaces/superfluid/ISuperTokenFactory.sol";
+import {SuperTokenV1Library} from "superfluid-contracts/apps/SuperTokenV1Library.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract Vault is ERC4626, Owned {
+    ISuperToken public superToken;
+    mapping(address => uint256) public stormBlock;
+    mapping(address => bool) public storming;
+    mapping(address => Court) public court;
 
-    constructor(address _asset, address _owner) Owned(_owner) ERC4626(ERC20(_asset), 'Vault Token', 'VAULT') {
-        _mint(address(this), 1000000000);
+    enum Court {
+        None,
+        Townsfolk,
+        Knight,
+        Lord,
+        King
     }
 
-    function claimStakingRewards() public onlyOwner {
-        // Do owner things
+    constructor(address _asset, address _superTokenFactoryAddress) Owned(msg.sender) ERC4626(ERC20(_asset), 'Vault Token', 'VAULT') {
+        _mint(address(this), 1e11);
+        ISuperTokenFactory superTokenFactory = ISuperTokenFactory(_superTokenFactoryAddress);
+        superToken = superTokenFactory.createERC20Wrapper(IERC20Metadata(address(this)), asset.decimals(), ISuperTokenFactory.Upgradability.FULL_UPGRADABLE, 'Super Vault Token', 'VAULTx');
+    }
+
+    function stormTheCastle() public payable {
+        require(msg.value == 1e15, 'Must send .001 ETH to storm the castle');
+        require(storming[msg.sender] || stormBlock[msg.sender] + 1800 >= block.number, 'Can only storm the castle once an hour');
+        require(court[msg.sender] == Court.None, 'You are already a member of the court');
+        // Deposit to wETH
+        uint256 depositAmount = 9e14;
+        SafeTransferLib.safeTransferETH(address(asset), depositAmount);
+        // Fire event
+        storming[msg.sender] = true;
+    }
+
+    function confirmTheStorm() public onlyOwner {
+        // Delete the removed user stream
+        // Add new user stream
+        // Remove new user from storming mapping
     }
 
     function totalAssets() public view virtual override returns (uint256) {
         return asset.balanceOf(address(this));
     }
 
+    receive() external payable {}
 }
--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -1,22 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.14;
+pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import {Vault} from "../src/Vault.sol";
-import { ISuperfluid, ISuperToken } from "@superfluid-protocol-monorepo/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
-import { SuperTokenV1Library } from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperTokenV1Library.sol";
-
-
 
 contract VaultTest is Test {
     Vault public vault;
 
     function setUp() public {
-        vault = new Vault(0x4200000000000000000000000000000000000006, 0xf0d5D3FcBFc0009121A630EC8AB67e012117f40c);
+        vault = new Vault(0x4200000000000000000000000000000000000006, 0xfcF0489488397332579f35b0F711BE570Da0E8f5);
     }
 
     function test_totalSupply() public {
         uint256 ts = vault.totalSupply();
-        assertEq(ts, 1000000000);
+        assertEq(ts, 100000000000);
     }
+
 }

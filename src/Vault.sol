c834e7345be6d45ef86059b287beef99140de610
Rory Hellyer <rory@webmodularity.com>
2024-03-19 20:28:53 -0700

    testing

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,48 +1,77 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "solmate/tokens/ERC4626.sol";
-import "solmate/auth/Owned.sol";
-import "solmate/tokens/ERC20.sol";
-import "solmate/utils/SafeTransferLib.sol";
+import {ERC4626} from "solmate/tokens/ERC4626.sol";
+import {Owned} from "solmate/auth/Owned.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 import {ISuperfluid, ISuperToken} from "superfluid-contracts/interfaces/superfluid/ISuperfluid.sol";
+import {SuperTokenV1Library} from "superfluid-contracts/apps/SuperTokenV1Library.sol";
 import {ISuperTokenFactory} from "superfluid-contracts/interfaces/superfluid/ISuperTokenFactory.sol";
 import {SuperTokenV1Library} from "superfluid-contracts/apps/SuperTokenV1Library.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract Vault is ERC4626, Owned {
-    ISuperToken public superToken;
+    using SuperTokenV1Library for ISuperToken;
+    ISuperToken public immutable superToken;
+    int96 public immutable totalFlowRate;
+    uint256 public immutable minStormPrice;
     mapping(address => uint256) public stormBlock;
     mapping(address => bool) public storming;
-    mapping(address => Court) public court;
-
-    enum Court {
+    mapping(address => CourtRole) public courtRoles;
+    // Court
+    address public king;
+    address[2] public lords;
+    address[3] public knights;
+    address[4] public townsfolk;
+    // Role
+    enum CourtRole {
         None,
         Townsfolk,
         Knight,
         Lord,
         King
     }
-    event StormTheCastle(address indexed stromAddress);
+    // Events
+    event StormTheCastle(address indexed stormAddress, uint256 indexed amountSent);
+    // Custom Errors
+    error BadAddress(address badAddress);
+    error GameEnded();
+    error InsufficientFunds(uint256 valueSent);
+    error TooFrequentStorms(uint256 nextBlockAllowed, uint256 currentBlockNumber, bool currentlyStorming);
+    error AlreadyCourtMember(CourtRole courtRole);
+    error TooMuchFlow(uint256 totalFlowRate);
 
-    constructor(address _asset, address _superTokenFactoryAddress) Owned(msg.sender) ERC4626(ERC20(_asset), 'Vault Token', 'VAULT') {
-        _mint(address(this), 1e11);
-        ISuperTokenFactory superTokenFactory = ISuperTokenFactory(_superTokenFactoryAddress);
-        superToken = superTokenFactory.createERC20Wrapper(IERC20Metadata(address(this)), asset.decimals(), ISuperTokenFactory.Upgradability.FULL_UPGRADABLE, 'Super Vault Token', 'VAULTx');
+    constructor(address _asset, address _superTokenFactoryAddress, uint256 _gameDurationDays, uint256 _totalMint, uint256 _minStormPrice) Owned(msg.sender) ERC4626(ERC20(_asset), 'Vault Token', 'VAULT') {
+        minStormPrice = _minStormPrice;
+        _mint(address(this), _totalMint);
+        superToken = ISuperTokenFactory(_superTokenFactoryAddress).createERC20Wrapper(IERC20Metadata(address(this)), asset.decimals(), ISuperTokenFactory.Upgradability.FULL_UPGRADABLE, 'Super Vault Token', 'VAULTx');
+        uint256 _totalFlowRate = (_totalMint / _gameDurationDays) * (24 * 60 * 60);
+        if (_totalFlowRate > uint256(uint96(type(int96).max))) revert TooMuchFlow(_totalFlowRate);
+        totalFlowRate = int96(uint96(_totalFlowRate));
+    }
+
+    function initGame(address _king, address[2] memory _lords, address[3] memory _knights, address[4] memory _townsfolk) public onlyOwner {
+        this.approve(address(superToken), this.totalSupply());
+        superToken.upgrade(this.totalSupply());
+        confirmTheStorm(_king, _lords, _knights, _townsfolk);
     }
 
     function stormTheCastle() public payable {
-        require(msg.value == 1e15, 'Must send .001 ETH to storm the castle');
-        require(storming[msg.sender] || stormBlock[msg.sender] + 1800 >= block.number, 'Can only storm the castle once an hour');
-        require(court[msg.sender] == Court.None, 'You are already a member of the court');
-        // Deposit to wETH
-        uint256 depositAmount = 9e14;
+        if (msg.sender == address(0)) revert BadAddress(msg.sender);
+        if (totalSupply <= 0) revert GameEnded();
+        if (msg.value < minStormPrice) revert InsufficientFunds(msg.value);
+        if (storming[msg.sender] || stormBlock[msg.sender] + 1800 >= block.number) revert TooFrequentStorms(stormBlock[msg.sender] + 1800, block.number, storming[msg.sender]);
+        if (courtRoles[msg.sender] != CourtRole.None) revert AlreadyCourtMember(courtRoles[msg.sender]);
         storming[msg.sender] = true;
-        SafeTransferLib.safeTransferETH(address(asset), depositAmount);
-        emit StormTheCastle(msg.sender);
+        // Deposit to wETH
+        SafeTransferLib.safeTransferETH(address(asset), msg.value - 1e14);
+        emit StormTheCastle(msg.sender, msg.value);
     }
 
-    function confirmTheStorm() public onlyOwner {
+    function confirmTheStorm(address _king, address[2] memory _lords, address[3] memory _knights, address[4] memory _townsfolk) public onlyOwner {
+        int96 flowRate = 1e18;
+        superToken.createFlow(_king, flowRate);
         // Delete the removed user stream
         // Add new user stream
         // Remove new user from storming mapping
@@ -50,6 +79,10 @@ contract Vault is ERC4626, Owned {
 
     function totalAssets() public view virtual override returns (uint256) {
         return asset.balanceOf(address(this));
+    }
+
+    function addToCourt(address accountAddress, CourtRole courtRole) private {
+
     }
 
     receive() external payable {}
--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -3,17 +3,54 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import {Vault} from "../src/Vault.sol";
+import {ISuperfluid, ISuperToken} from "superfluid-contracts/interfaces/superfluid/ISuperfluid.sol";
 
 contract VaultTest is Test {
     Vault public vault;
+    address public accountAddress = address(12345);
+    event StormTheCastle(address indexed stormAddress, uint256 indexed amountSent);
 
     function setUp() public {
-        vault = new Vault(0x4200000000000000000000000000000000000006, 0xfcF0489488397332579f35b0F711BE570Da0E8f5);
+        vault = new Vault(
+            0x4200000000000000000000000000000000000006,
+            0xfcF0489488397332579f35b0F711BE570Da0E8f5,
+            30,
+            1e24,
+            1e15
+        );
+        vault.initGame(
+            address(1337),
+            [address(11), address(12)],
+            [address(13), address(14), address(15)],
+            [address(16), address(17), address(18), address(19)]
+        );
     }
 
     function test_totalSupply() public {
         uint256 ts = vault.totalSupply();
-        assertEq(ts, 100000000000);
+        assertEq(ts, 1e24);
+    }
+
+    function test_flowRate() public {
+        console.logInt(vault.totalFlowRate());
+    }
+
+    function test_stormTheCastle() public {
+        uint256 totalAssetsStart = vault.totalAssets();
+        uint256 totalBalanceStart = address(vault).balance;
+        // Switch to accountAddress
+        hoax(accountAddress);
+        // Event
+        vm.expectEmit(true, true, false, false);
+        emit StormTheCastle(accountAddress, 1e15);
+        // Storm the castle
+        vault.stormTheCastle{value: 1e15}();
+        // Check protocol fee as native
+        assertEq(address(vault).balance, totalBalanceStart + 1e14);
+        // Check wETH wrapped and added to vault
+        assertEq(vault.totalAssets(), totalAssetsStart + 9e14);
+        // Make sure account is flagged as storming
+        assertEq(vault.storming(accountAddress), true);
     }
 
 }
